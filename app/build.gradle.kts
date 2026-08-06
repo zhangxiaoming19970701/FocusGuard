bc0315a505fe5d73aeedf55a5b70cbc464c46e90
zhangxiaoming19970701 <zhangxiaoming19970701@gmail.com>
2026-08-06 15:44:13 +0800

    Fix Kotlin JVM target configuration
    Updated Kotlin options to use the new compiler options syntax for setting JVM target.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,4 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
@@ -35,12 +36,17 @@ android {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
-    kotlinOptions { jvmTarget = "17" }
 
     buildFeatures { compose = true }
 
     packaging {
         resources.excludes += "/META-INF/{AL2.0,LGPL2.1}"
+    }
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget.set(JvmTarget.JVM_17)
     }
 }
 
